7de4f40a297f045d04b14b85b83f5f909c4e38e2
bks1b <bks1bence@gmail.com>
2023-07-20 22:45:19 +0200

    Fixed a mobile issue

--- src/server/index.ts
+++ src/server/index.ts
@@ -44,7 +44,7 @@ export const getRouter = (config: Config) => {
 export const getHTML = (config: HTMLConfig & BaseConfig) => `<!DOCTYPE html>
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, maximum-scale=1.0">
         <link rel="stylesheet" href="${config.rootPath || ''}/baseStyle.css">
         ${config.parser ? `<link rel="stylesheet" href="${config.rootPath || ''}/parser.css">` : ''}
         ${
